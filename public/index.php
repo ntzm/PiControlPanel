b3d372ffa1da446e156a3d510bb5353e51eb0ba5
Nat Zimmermann <nat@natzim.me>
2015-03-15 20:54:42 +0000

    Changed CDN for font awesome

--- public/index.php
+++ public/index.php
@@ -9,7 +9,7 @@ include_once('../pi.php');
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://bootswatch.com/darkly/bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css">
 </head>
 <body>
     <nav class="navbar navbar-default">
